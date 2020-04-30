5f172a807525e445247e116b9b7af4d09515f002
Michael-Xie <xiemicha@gmail.com>
2020-04-29 22:38:02 -0400

    Link form input for url to videoplayer

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import YouTube from 'react-youtube';
 import ReactPlayer from 'react-player';
 import { Button, Input } from '@material-ui/core';
 
 const VideoPlayer = function({props}) {
-
+    const [url, setUrl] = useState('https://www.youtube.com/watch?v=ysz5S6PUM-U');
+    // 'https://www.youtube.com/watch?v=ysz5S6PUM-U'
     const opts = {
         height: '390',
         width: '640',
@@ -17,14 +18,30 @@ const VideoPlayer = function({props}) {
         // access to player in all event handlers via event.target
         // event.target.pauseVideo();
         console.log("onReady", event);
+    }
+
+    const _onSubmit = function(event) {
+        console.log("before Link", event.target);
+
+        event.preventDefault();
+        console.log("Link", event.target);
+        setUrl(event.target.value);
+    }
+    const handleOnChange = (e) => {
+        console.log(e.target.value);
+        setUrl(e.target.value);
+    };
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e);
       }
 
     return (
         <>
-            <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' controls onReady={_onReady}/>
-            <form>
-                <Input placeholder="Enter Link" inputProps={{ 'aria-label': 'Enter Link' }} required/>
-                <Button variant="contained" color="primary">Load</Button>
+            <ReactPlayer url={url} controls onReady={_onReady}/>
+            <form id="changeVideoForm" onSubmit={handleSubmit}>
+                <Input type="text" placeholder="Enter a video link and watch with friends..." name="link" value={url} onChange={handleOnChange} defaultValue="https://"/>
             </form>
         </>
 
