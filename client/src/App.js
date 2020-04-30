dba9d9952eed5aa1a5f16607e2b695cc92717e17
Michael-Xie <xiemicha@gmail.com>
2020-04-30 15:02:52 -0400

    Pass twilio sync to VideoPlayer

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,35 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import VideoPlayer from "./components/VideoPlayer";
 import axios from "axios";
-const SyncClient = require('twilio-sync');
+const twilioSync = require('twilio-sync');
 
 const SERVER_URL = "http://localhost:3001";
 function App() {
-
+  const [sync, setSync] = useState("");
+  
   useEffect(()=>{
     axios.get(`${SERVER_URL}/token`)
     .then((tokenResponse) => {
       console.log("tokenResponse", tokenResponse);
-      const syncClient = new SyncClient(tokenResponse.data.token, { logLevel: 'info' });
+      const syncClient = new twilioSync(tokenResponse.data.token, { logLevel: 'info' });
       syncClient.on('connectionStateChanged', function(state) {
         if (state !== 'connected') {
           console.log('Sync is not live (websocket connection <span style="color: red">' + state + '</span>)…');
         } else {
           console.log('Sync is live!');
+          setSync(syncClient);
         }
       })
     })
     .catch((err) => console.log(err));
   }, []);
 
-  
-
+  console.log('sync app', sync)
 
   return (
     <div className="App">
-      <VideoPlayer/>
+      <VideoPlayer twilioSync={sync}/>
     </div>
   );
 }
