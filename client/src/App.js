d3e446e3571c0584ec044411732801594e2ffe9f
Michael-Xie <xiemicha@gmail.com>
2020-05-01 02:32:46 -0400

    Move states up and add hook to abstract updateVideoState and syncVideo

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import VideoPlayer from "./components/VideoPlayer";
+import {useVideoPlayerSync} from "../src/hooks/useVideoPlayerSync";
+import VideoPlayerSync from "./components/VideoPlayerSync";
 import axios from "axios";
 const twilioSync = require('twilio-sync');
 
+
+
 const SERVER_URL = "http://localhost:3001";
+
 function App() {
+  const initVideoState = {
+    url: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    playing: false,
+  };
+  const [videoState, setVideoState] = useState(initVideoState);
   const [sync, setSync] = useState("");
-  
+  const {updateVideoState, syncVideo} = useVideoPlayerSync(videoState, setVideoState, sync);
+
   useEffect(()=>{
     axios.get(`${SERVER_URL}/token`)
     .then((tokenResponse) => {
@@ -25,24 +35,44 @@ function App() {
     .catch((err) => console.log(err));
   }, []);
 
-  console.log('sync app', sync)
-  // initialize sync doc for video state
-  const initVideoState = {
-    url: "https://",
-    playing: false
-  };
+  // initialize sync doc for video state and video time
   useEffect(()=> {
     if (sync) {
       sync.document("videoState")
       .then((syncDoc) => {
-        syncDoc.set(initVideoState);
+        syncDoc.set(videoState);
+      })
+      sync.document("videoTime")
+      .then((syncDoc) => {
+        syncDoc.set({playedSeconds: 0})
       })
     }
   }, [sync]);
 
+  // get video state changes from other client(s)
+  useEffect(() => {
+      if (sync) {
+          sync.document("videoState")
+          .then((syncDoc) => {
+              syncDoc.on("updated", (event) => {
+                  // console.log("Updated", event.isLocal? "locally." : "by the other guy.");
+                  // console.log("updated sync value", event.value);
+                  setVideoState((prev) => ({...prev, ...event.value}));
+                  // if (myRef && myRef.current) {
+                  //     myRef.current.seekTo(event.value.played, "seconds");
+                  // }
+              })
+
+          })
+      }
+  });
   return (
     <div className="App">
-      <VideoPlayer twilioSync={sync} initVideoState={initVideoState}/>
+      <VideoPlayerSync 
+        videoState={videoState} 
+        updateVideoState={updateVideoState}
+        syncVideo={syncVideo}
+      />
     </div>
   );
 }
--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,104 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 // import YouTube from 'react-youtube';
 import ReactPlayer from 'react-player';
 import { Button, Input } from '@material-ui/core';
 
-const VideoPlayer = function({twilioSync, initVideoState}) {
-    // currentVideo={videoStates[channel].url}
-    // changeVideoState={changeVideoState}
-    // paused={videoStates[channel] ? videoStates[channel].paused : false}
-    // played={videoStates[channel] ? videoStates[channel].played : true}
-    // timeStamp={videoStates[channel] ? videoStates[channel].timeStamp : 0}
-    // channel={channel}
-    // syncVideo={syncVideo}
-    const [url, setUrl] = useState('https://www.youtube.com/watch?v=ysz5S6PUM-U');
-    // const initVideoState = async function(twilioSync) {
-    //     if (twilioSync) {
-    //         const syncDoc = await twilioSync.document("videoState");
-    //         console.log("initialized", syncDoc.value);
-    //         return syncDoc.value;
-    //     }
-    // }
+const VideoPlayer = function({url, playing, playedSeconds, updateVideoState, syncVideo}) {
 
-    const [videoState, setVideoState] = useState(initVideoState);
+    const myRef = useRef();
 
     // 'https://www.youtube.com/watch?v=ysz5S6PUM-U'
 
+
+
     useEffect(() => {
-        if (twilioSync) {
-            twilioSync.document("videoState")
-            .then((syncDoc) => {
-                syncDoc.on("updated", (event) => {
-                    console.log("Updated", event.isLocal? "locally." : "by the other guy.");
-                    console.log("updated sync value", event.value);
-                    setVideoState(event.value);
-                })
-
-            })
+        // myRef.current.seekTo(videoState.playedSeconds, "seconds");
+        if (playedSeconds && myRef && myRef.current) {
+            console.log('current time', myRef.current.getCurrentTime(), 'state time', playedSeconds);
+            // if (Math.abs(myRef.current.getCurrentTime() - playedSeconds) > 0) {
+            //     myRef.current.seekTo(playSeconds, "seconds");
+            // }
         }
-    });
+    }, [playedSeconds])
 
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-        }
-    };
-    const _onReady = function(event) {
-        // access to player in all event handlers via event.target
-        // event.target.pauseVideo();
-        console.log("onReady", event);
-        // twilioSync.document('videoState')
-        // .then((syncDoc) => {
-        //     syncDoc.set(videoState);
-        //     console.log(videoState)
-        // });
-    }
+
+    // const opts = {
+    //     height: '390',
+    //     width: '640',
+    //     playerVars: {
+    //       // https://developers.google.com/youtube/player_parameters
+    //       autoplay: 1,
+    //     }
+    // };
     
+    const onReady = function(event) {
+        console.log("ready", event);
+    }
     const onPlay = function(event) {
         console.log("playing...");
-        // setVideoState((prev) => {
-        //     return {...prev,
-        //     playing: true}
-        // });
-        twilioSync.document('videoState')
-        .then((syncDoc) => {
-            syncDoc.update({playing: true});
-            console.log(videoState)
-        });
+        updateVideoState(url, true);
     }
     const onPause = function(event) {
         console.log("pausing...");
-        // setVideoState((prev) => {
-        //     return {...prev,
-        //     playing: false}
-        // });
-        twilioSync.document('videoState')
-        .then((syncDoc) => {
-            syncDoc.update({playing: false});
-            console.log(videoState)
-        });
+        updateVideoState(url, false);
     }
 
-    const handleOnChange = (e) => {
-        console.log(e.target.value);
-        setUrl(e.target.value);
-    };
+
     
-    const onProgress = (e) => {
-        console.log("progress", e);
-    };    
-
     return (
-        <>
-            <ReactPlayer url={url} controls playing={videoState.playing} onReady={_onReady} onPlay={onPlay} onPause={onPause} onProgress={onProgress}/>
-            <form id="changeVideoForm" onSubmit={(e) => e.preventDefault()}>
-                <Input type="text" placeholder="Enter a video link and watch with friends..." name="link" value={url} onChange={handleOnChange} defaultValue="https://"/>
-            </form>
-        </>
-
+        <ReactPlayer ref={myRef} url={url} controls playing={playing} onPlay={onPlay} onPause={onPause} onReady={onReady}/>
     )
 }
 
